c9648e27c74055d205ce8445ec9c4a8eef90d138
ZINAN <97530806+Zinan100@users.noreply.github.com>
2022-06-21 21:27:08 +0530

    Create callbacks.py

---
+++ plugins/callbacks.py
@@ -0,0 +1,53 @@
+from pyrogram import Client as mrbots, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+
+ABOUT_TXT = """✮ 𝙼𝚈 𝙽𝙰𝙼𝙴: ᴍʀ. ʀᴇɴᴀᴍᴇʀ ᴜʟᴛʀᴏɴ
+✮ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: 𝚉𝙸𝙽𝙰𝙽
+✮ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
+✮ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
+✮ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾-𝙳𝙱
+✮ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
+✮ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: 𝚅1.0.43
+✮ 𝚄𝙿𝙳𝙰𝚃𝙴𝚂: 𝙼𝚂 𝙼𝙾𝚅𝙸𝙴𝚂 𝙾𝙵𝙵𝙸𝙲𝙸𝙰𝙻𝚂"""
+
+HELP_TXT = """𝗛𝗘𝗟𝗣 𝗠𝗘𝗡𝗨 𝗙𝗢𝗥 𝗧𝗛𝗘 𝗕𝗢𝗧
+Tᴏ Dᴇʟ Tʜᴜᴍᴘɴᴀɪʟ /Delthumb
+Tᴏ Sᴇᴇ Tʜᴜᴍʙ /viewthumb"""
+
+
+
+
+@mrbots.on_callback_query()
+async def cb_handler(client, query: CallbackQuery):
+    if data == "start":
+        await query.message.edit_text(
+            text=f"""👋 Hai {query.from_user.mention} \n𝙸'𝚖 𝙰 𝚂𝚒𝚖𝚙𝚕𝚎 𝙵𝚒𝚕𝚎 𝚁𝚎𝚗𝚊𝚖𝚎+𝙵𝚒𝚕𝚎 𝚃𝚘 𝚅𝚒𝚍𝚎𝚘 𝙲𝚘𝚟𝚎𝚛𝚝𝚎𝚛 𝙱𝙾𝚃 𝚆𝚒𝚝𝚑 𝙿𝚎𝚛𝚖𝚊𝚗𝚎𝚗𝚝 𝚃𝚑𝚞𝚖𝚋𝚗𝚊𝚒𝚕 & 𝙲𝚞𝚜𝚝𝚘𝚖 𝙲𝚊𝚙𝚝𝚒𝚘𝚗 𝚂𝚞𝚙𝚙𝚘𝚛𝚝! """,
+            reply_markup=InlineKeyboardMarkup( [[
+                InlineKeyboardButton('📢 𝚄𝙿𝙳𝙰𝚃𝙴𝚂', url='https://t.me/mr_BOTS_tg'),
+                InlineKeyboardButton('🍂 𝚂𝚄𝙿𝙿𝙾𝚁𝚃', url='https://t.me/mr_BOTS_chats')
+                ],[
+                InlineKeyboardButton('🍃 𝙰𝙱𝙾𝚄𝚃', callback_data='about'),
+                InlineKeyboardButton('ℹ️ 𝙷𝙴𝙻𝙿', callback_data='help')
+               ]]
+              )
+        )
+        return
+    elif data == "help":
+        await query.message.edit_text(
+            text=HELP_TXT,
+            reply_markup=InlineKeyboardMarkup( [[
+               InlineKeyboardButton("🔒 𝙲𝙻𝙾𝚂𝙴", callback_data = "close"),
+               InlineKeyboardButton("◀️ 𝙱𝙰𝙲𝙺", callback_data = "start")
+               ]]
+            )
+        )
+    elif data == "about":
+        await query.message.edit_text(
+            text=ABOUT_TXT.format(client.mention),
+            disable_web_page_preview = True,
+            reply_markup=InlineKeyboardMarkup( [[
+               InlineKeyboardButton("🔒 𝙲𝙻𝙾𝚂𝙴", callback_data = "close"),
+               InlineKeyboardButton("◀️ 𝙱𝙰𝙲𝙺", callback_data = "start")
+               ]]
+            )
+        )
